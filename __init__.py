7eeb605b322ac9afcbea6f7ab3e00a6bdef7301c
Liyang <liyang@holly.home>
2024-05-27 12:30:28 +0800

    Maxlen, __iter__, doc

--- __init__.py
+++ __init__.py
@@ -1,48 +1,64 @@
 from functools import partial
-from PyQt5.QtCore import pyqtSlot
-from PyQt5.QtWidgets import QAction
 
 
 class RecentItemsList:
 	"""
-	Use for filling item files, (or other things).
-	The most item selected is always returned first in the list.
-	Maximum length is enforced.
+	A list which is ordered by the most recently selected item.
 	Items may be removed, such as when a file is missing.
+
+	Example:
+
+	in __init__:
+
+		self._recent_files = RecentItemsList(self.settings.value("recent_files", defaultValue=[]))
+		self.menuOpen_Recent.aboutToShow.connect(self.fill_recent_files)
+
+	Filling a menu:
+
+		@pyqtSlot()
+		def fill_recent_files(self):
+			self.menuOpen_Recent.clear()
+			actions = []
+			for filename in self._recent_files:
+				action = QAction(filename, self)
+				action.triggered.connect(partial(self.load_soundfont, filename))
+				actions.append(action)
+			self.menuOpen_Recent.addActions(actions)
+
+
+	When item is found:
+
+		self._recent_files.select(filename)
+
+	When item is missing:
+
+		self._recent_files.remove(filename)
+
+	Save:
+
+		self.settings.setValue("recent_files", self._recent_files.items)
+
 	"""
+
+	maxlen = 0
 
 	def __init__(self, items):
 		self.items = items
 
 	def select(self, item):
 		"""
-		Shifts the given item to the top of the list
+		Insert the given item to the top of the list.
+		If the given item already exists, shifts the given item to the top.
 		"""
 		old_items = self.items
 		self.items = [item]
 		self.items.extend([old_item for old_item in old_items if old_item != item])
+		if self.maxlen > 0:
+			self.items = self.items[:self.maxlen]
 
 	def remove(self, item):
 		self.items = [old_item for old_item in self.items if old_item != item]
 
-	@pyqtSlot()
-	def fill_menu(self, menu, trigger_function):
-		"""
-		Convenience function for PyQt when "aboutToShow" is triggered.
-
-			self.menuOpen_Recent.aboutToShow.connect(self.fill_recent_files)
-
-		Pass a form's menu object as the first parameter.
-
-		The "trigger_function" is a method in your window class which is called when
-		the user makes a selection. The callback is passed the value of the item
-		selected.
-		"""
-		menu.clear()
-		actions = []
-		for item in self.items:
-			action = QAction(item, self)
-			action.triggered.connect(partial(trigger_function, item))
-			actions.append(action)
-		menu.addActions(actions)
+	def __iter__(self):
+		return self.items.__iter__()
 
